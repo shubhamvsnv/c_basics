287d03b9f7cf8494b5f52857822be55e20979e68
Shubham Vaishnav <shubhamvsnv267@gmail.com>
2020-09-30 19:00:54 +0530

    Update 09-getchar_example.c

--- 09-getchar_example.c
+++ 09-getchar_example.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include<stdio.h> 		// stdio Header file
+#include<stdlib.h>		// stdio Header file
 
 int main() 
 {
-	printf("Please enter a char \n");
-	char c = getchar();
-	getchar(); // for the newline
+	printf("Please enter a char \n");		// Asking user to enter a character 
+	char c = getchar();				// getting user character input in c variable
+	getchar(); 					// for the newline
 	
-	printf("The user entered %c, what a character \n",c);
+	printf("The user entered %c, what a character \n",c);	// showing output to user that user enter this character
 
 	return 0;
 	
