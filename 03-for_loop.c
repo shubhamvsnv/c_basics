790b0a655a3b2c81ff99bd1ef4f9d5bbf0350cd9
Shubham Vaishnav <shubhamvsnv267@gmail.com>
2020-09-30 18:49:53 +0530

    Update 03-for_loop.c

--- 03-for_loop.c
+++ 03-for_loop.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include<stdio.h>			// stdio Header file
+#include<stdlib.h>			// stdlib Header file
 
 int main() {
 
-	printf("Hello World\n");
-	int i;
-	for(i=0; i<10; i++)
+	printf("Hello World\n");	// print simple Hello msg
+	int i;				// i is initialized to 1
+	for(i=0; i<10; i++)		// for( initial value ; end condition ; increment of i )
 	{
-		printf("In a loop\n");
+		printf("In a loop\n");	// Printing msg which show's when loop executing 10 times
 	}
 	
 	return EXIT_SUCCESS;
